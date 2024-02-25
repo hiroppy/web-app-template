568da871b9a664a6673c038af05a0661d7080071
Yuta Hiroto <git@hiroppy.me>
2024-02-26 08:44:18 +0900

    refactor: modify init.mjs

--- init.mjs
+++ init.mjs
@@ -1,73 +1,72 @@
 import { spawn } from "node:child_process";
 import { readFile, unlink, writeFile } from "node:fs/promises";
 import { basename } from "node:path";
+import { createInterface } from "node:readline/promises";
 
-await Promise.all([removeLines(), commands(), updatePackageJson()]);
-await unlink(new URL(import.meta.url).pathname);
+const fences = [
+  ["####### 👉 remove #######", "########################"],
+  ["<!-- 👉 remove -->", "<!-- ######## -->"],
+];
 
-await updatePackageJson();
-
-async function removeLines() {
-  async function removeLinesFromFile(filePath, fence) {
-    console.log(`removing lines from: ${filePath}`);
-
-    try {
-      const data = await readFile(filePath, "utf8");
-      const lines = data.split("\n");
-      const res = [];
-      let isInFence = false;
-
-      for (const line of lines) {
-        if (line.trim() === fence[0]) {
-          isInFence = true;
-        }
-        if (!isInFence) {
-          res.push(line);
-        }
-        if (line.trim() === fence[1]) {
-          isInFence = false;
-        }
-      }
-
-      await writeFile(filePath, res.join("\n"));
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  const fences = [
-    ["####### 👉 remove #######", "########################"],
-    ["<!-- 👉 remove -->", "<!-- ######## -->"],
-  ];
-  const files = [
+await Promise.all([
+  removeLines([
     [".gitignore", fences[0]],
     [".github/workflows/ci.yml", fences[0]],
     ["README.md", fences[1]],
-  ];
+  ]),
+  generateMigrationFiles(),
+  updatePackageJson(),
+]);
+
+// remove me
+await unlink(new URL(import.meta.url).pathname);
+
+await format();
+
+async function removeLines(files) {
+  title("removing lines");
 
   await Promise.all(
-    files.map(async ([file, fence]) => await removeLinesFromFile(file, fence)),
+    files.map(async ([file, fence]) => {
+      try {
+        const filePath = new URL(file, import.meta.url);
+        const data = await readFile(filePath, "utf8");
+        const lines = data.split("\n");
+        const res = [];
+        let isInFence = false;
+
+        for (const line of lines) {
+          if (line.trim() === fence[0]) {
+            isInFence = true;
+          }
+          if (!isInFence) {
+            res.push(line);
+          }
+          if (line.trim() === fence[1]) {
+            isInFence = false;
+          }
+        }
+
+        await writeFile(filePath, res.join("\n"));
+      } catch (error) {
+        console.error(error);
+      }
+    }),
   );
 }
 
-async function commands() {
-  const commands = ["pnpm dev:db:setup", "pnpm fmt", "pnpm db:stop"];
+async function removeFiles(files) {}
+
+async function generateMigrationFiles() {
+  title("creating migration files");
+
+  const commands = ["pnpm dev:db:setup", "pnpm db:stop"];
 
   for (const command of commands) {
-    console.log(`running: ${command}`);
-
     const [cmd, ...args] = command.split(" ");
 
     await new Promise((resolve, reject) => {
       const child = spawn(cmd, args, { stdio: "inherit" });
-
-      child.stdout?.on("data", (data) => {
-        console.log(data);
-      });
-
-      child.stderr?.on("data", (data) => {
-        console.log(data);
-      });
 
       child.on("exit", (code) => {
         if (code === 0) {
@@ -80,7 +79,25 @@ async function commands() {
   }
 }
 
+async function format() {
+  title("formatting");
+
+  await new Promise((resolve, reject) => {
+    const child = spawn("pnpm", ["fmt"], { stdio: "inherit" });
+
+    child.on("exit", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`command failed with code ${code}`));
+      }
+    });
+  });
+}
+
 async function updatePackageJson() {
+  title("updating package.json");
+
   const packageJsonPath = new URL("./package.json", import.meta.url);
   const packageJson = await readFile(packageJsonPath, "utf8");
   const parsed = JSON.parse(packageJson);
@@ -89,4 +106,8 @@ async function updatePackageJson() {
   parsed.name = currentDirectoryName;
 
   await writeFile(packageJsonPath, JSON.stringify(parsed, null, 2));
+}
+
+function title(title) {
+  console.log("\x1b[36m%s\x1b[0m", `🎃 - ${title}`);
 }
