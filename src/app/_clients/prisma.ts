263c432c33b01eddce14c7c5174199dfe5666ae2
Yuta Hiroto <git@hiroppy.me>
2024-02-25 18:33:36 +0900

    fix: remove prisma from global.d.ts

--- src/app/_clients/prisma.ts
+++ src/app/_clients/prisma.ts
@@ -7,9 +7,12 @@ export let prisma: PrismaClient;
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
+  // @ts-expect-error avoid declaring global.prisma at global.d.ts to enable auto import by editors
   if (!global.prisma) {
+    // @ts-expect-error ditto
     global.prisma = new PrismaClient();
   }
 
+  // @ts-expect-error ditto
   prisma = global.prisma;
 }
--- src/app/globals.d.ts
+++ src/app/globals.d.ts
@@ -2,9 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import type { DefaultUser } from "next-auth";
 
 declare global {
-  // biome-ignore lint: noVar
-  var prisma: PrismaClient;
-
   namespace NodeJS {
     interface ProcessEnv {
       NEXT_PUBLIC_SITE_URL: string;
