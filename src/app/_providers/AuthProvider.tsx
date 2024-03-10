85ca99ec1c5ef7328b07a58d0f163c4b6c926b30
Yuta Hiroto <git@hiroppy.me>
2024-03-10 12:21:45 +0900

    refactor: by biome

--- src/app/_providers/AuthProvider.tsx
+++ src/app/_providers/AuthProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { type PropsWithChildren } from "react";
+import type { PropsWithChildren } from "react";
 
 export function AuthProvider({ children }: PropsWithChildren) {
   return <SessionProvider>{children}</SessionProvider>;
