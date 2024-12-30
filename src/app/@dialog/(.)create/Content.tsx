b210924008af2fe44155211474f1b3195389610c
Yuta Hiroto <git@hiroppy.me>
2024-12-31 07:41:24 +0900

    fix

---
+++ src/app/@dialog/(.)create/Content.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { create } from "@/app/_actions/items";
+import { type ItemCreateSchema, itemCreateSchema } from "@/app/_schemas/items";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import { type SubmitHandler, useForm } from "react-hook-form";
+import { Dialog } from "../_components/Dialog";
+
+// users who are not logged in cannot reach here due to intercepting routes.
+export function Content() {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<ItemCreateSchema>({
+    mode: "onChange",
+    resolver: zodResolver(itemCreateSchema),
+  });
+
+  const submit: SubmitHandler<ItemCreateSchema> = async (values) => {
+    if (isPending) {
+      return;
+    }
+
+    startTransition(async () => {
+      try {
+        await create(values);
+        router.push("/");
+      } catch (e) {
+        if (e instanceof Error) {
+          alert(e.message);
+        }
+      }
+    });
+  };
+
+  return (
+    <Dialog>
+      <form onSubmit={handleSubmit(submit)} className="space-y-4">
+        <label htmlFor="content">New Memo</label>
+        <input
+          {...register("content")}
+          id="content"
+          disabled={isPending}
+          placeholder="write you memo..."
+          className=" w-full bg-gray-600 text-gray-100 focus:outline-none py-3 px-5 rounded-sm"
+          data-1p-ignore
+        />
+        {errors.content?.message && (
+          <span className="text-red-400">{errors.content.message}</span>
+        )}
+      </form>
+    </Dialog>
+  );
+}
--- src/app/@dialog/(.)create/page.tsx
+++ src/app/@dialog/(.)create/page.tsx
@@ -1,59 +1,8 @@
-"use client";
+import { Content } from "./Content";
 
-import { create } from "@/app/_actions/items";
-import { type ItemCreateSchema, itemCreateSchema } from "@/app/_schemas/items";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useRouter } from "next/navigation";
-import { useTransition } from "react";
-import { type SubmitHandler, useForm } from "react-hook-form";
-import { Dialog } from "../_components/Dialog";
+// bug: https://github.com/vercel/next.js/issues/74128
+export const dynamic = "force-dynamic";
 
-// users who are not logged in cannot reach here due to intercepting routes.
 export default function Page() {
-  const router = useRouter();
-  const [isPending, startTransition] = useTransition();
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<ItemCreateSchema>({
-    mode: "onChange",
-    resolver: zodResolver(itemCreateSchema),
-  });
-
-  const submit: SubmitHandler<ItemCreateSchema> = async (values) => {
-    if (isPending) {
-      return;
-    }
-
-    startTransition(async () => {
-      try {
-        await create(values);
-        router.push("/");
-      } catch (e) {
-        if (e instanceof Error) {
-          alert(e.message);
-        }
-      }
-    });
-  };
-
-  return (
-    <Dialog>
-      <form onSubmit={handleSubmit(submit)} className="space-y-4">
-        <label htmlFor="content">New Memo</label>
-        <input
-          {...register("content")}
-          id="content"
-          disabled={isPending}
-          placeholder="write you memo..."
-          className=" w-full bg-gray-600 text-gray-100 focus:outline-none py-3 px-5 rounded-sm"
-          data-1p-ignore
-        />
-        {errors.content?.message && (
-          <span className="text-red-400">{errors.content.message}</span>
-        )}
-      </form>
-    </Dialog>
-  );
+  return <Content />;
 }
