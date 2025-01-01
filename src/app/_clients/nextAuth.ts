65a9e3fe2df95cba4326bd20290283763e3d557f
Yuta Hiroto <git@hiroppy.me>
2025-01-01 17:03:48 +0900

    fix: control JWT of multiple devices (#353)

--- src/app/_clients/nextAuth.ts
+++ src/app/_clients/nextAuth.ts
@@ -8,12 +8,44 @@ export const {
   unstable_update: update,
   handlers,
 } = NextAuth({
+  ...config,
   // https://authjs.dev/getting-started/migrating-to-v5#edge-compatibility
   // @ts-expect-error https://github.com/nextauthjs/next-auth/issues/9493
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
+  callbacks: {
+    ...config.callbacks,
+    jwt: async ({ token, user, trigger }) => {
+      // user exists when only signing in
+      if (user) {
+        token.id = user.id;
+        token.role = user.role;
+      }
+
+      // support for multiple devices
+      const me = await prisma.user.findUnique({
+        where: {
+          id: token.id as string,
+        },
+      });
+
+      // to avoid accessing devices having invalid JWT if user is not found
+      if (trigger !== "signUp" && !user && !me) {
+        throw new Error("User not found");
+      }
+
+      // in favor of the user's latest data and update the token
+      if (me) {
+        token.name = me.name;
+        token.image = me.image;
+        token.email = me.email;
+        token.role = me.role;
+      }
+
+      return token;
+    },
+  },
   ...(process.env.NEXTAUTH_TEST_MODE === "true" ? configForTest : {}),
-  ...config,
 });
--- src/app/globals.d.ts
+++ src/app/globals.d.ts
@@ -25,3 +25,9 @@ declare module "next-auth" {
     user: User;
   }
 }
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+  }
+}
