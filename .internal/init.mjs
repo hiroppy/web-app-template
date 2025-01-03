518cf43fce4d158ace7e17ae8090ff2d6c5aa99b
Yuta Hiroto <git@hiroppy.me>
2025-01-03 14:24:46 +0900

    wip

--- .internal/init.mjs
+++ .internal/init.mjs
@@ -1,11 +1,20 @@
 import { spawn } from "node:child_process";
-import { readFile, rm, unlink, writeFile } from "node:fs/promises";
+import { readFile, rm, writeFile } from "node:fs/promises";
 import { basename, join, resolve } from "node:path";
 import { createInterface } from "node:readline/promises";
+import {
+  getPackageJson,
+  removeDirs,
+  removeFiles,
+  removeLines,
+  removeWords,
+  title,
+} from "./utils.mjs";
 
 const [_, __, ...flags] = process.argv;
 const isSkipQuestions = flags.includes("--skip-questions");
 const isRemoveDocker = flags.includes("--remove-docker");
+const isRemoveOtel = flags.includes("--remove-otel");
 const fences = [
   ["####### 👉 remove #######", "########################"],
   ["<!-- 👉 remove -->", "<!-- ######## -->"],
@@ -32,75 +41,19 @@ await format();
 
 console.log("done! please commit them 🐶");
 
-async function removeLines(files) {
-  title("removing lines");
+async function format() {
+  title("formatting");
+  await new Promise((resolve, reject) => {
+    const child = spawn("pnpm", ["fmt"], { stdio: "overlapped" });
 
-  await Promise.all(
-    files.map(async ([file, fence]) => {
-      try {
-        const target = join(basePath, file);
-        const data = await readFile(target, "utf8");
-        const lines = data.split("\n");
-        const res = [];
-        let isInFence = false;
-
-        for (const line of lines) {
-          if (line.trim() === fence[0]) {
-            isInFence = true;
-          }
-          if (!isInFence) {
-            res.push(line);
-          }
-          if (line.trim() === fence[1]) {
-            isInFence = false;
-          }
-        }
-
-        await writeFile(target, res.join("\n"));
-      } catch (error) {
-        console.error(error);
+    child.on("exit", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`command failed with code ${code}`));
       }
-    }),
-  );
-}
-
-async function removeWords(file, words) {
-  const target = join(basePath, file);
-  const data = await readFile(target, "utf8");
-  const lines = data.split("\n");
-  const res = [];
-
-  for (const line of lines) {
-    let str = line;
-
-    // keep already empty lines
-    if (str.trim() === "") {
-      res.push(str);
-      continue;
-    }
-
-    for (const word of words) {
-      if (line.includes(word)) {
-        str = str.replace(word, "");
-      }
-    }
-
-    if (str.trim() !== "") {
-      res.push(str);
-    }
-  }
-
-  await writeFile(target, res.join("\n"));
-}
-
-async function removeFiles(files) {
-  await Promise.all(files.map((file) => unlink(join(basePath, file))));
-}
-
-async function removeDirs(dirs) {
-  await Promise.all(
-    dirs.map((dir) => rm(join(basePath, dir), { recursive: true })),
-  );
+    });
+  });
 }
 
 async function generateMigrationFiles() {
@@ -130,34 +83,16 @@ async function generateMigrationFiles() {
   }
 }
 
-async function format() {
-  title("formatting");
-
-  await new Promise((resolve, reject) => {
-    const child = spawn("pnpm", ["fmt"], { stdio: "overlapped" });
-
-    child.on("exit", (code) => {
-      if (code === 0) {
-        resolve();
-      } else {
-        reject(new Error(`command failed with code ${code}`));
-      }
-    });
-  });
-}
-
 async function updatePackageJson() {
   title("updating package.json");
 
-  const packageJsonPath = join(basePath, "package.json");
-  const packageJson = await readFile(packageJsonPath, "utf8");
-  const parsed = JSON.parse(packageJson);
+  const { path, data } = await getPackageJson();
   const currentDirectoryName = basename(process.cwd());
 
-  parsed.name = currentDirectoryName;
-  parsed.version = "0.0.1";
+  data.name = currentDirectoryName;
+  data.version = "0.0.1";
 
-  await writeFile(packageJsonPath, JSON.stringify(parsed, null, 2));
+  await writeFile(path, JSON.stringify(data, null, 2));
 }
 
 async function docker() {
@@ -201,6 +136,4 @@ async function docker() {
   }
 }
 
-function title(title) {
-  console.log("\x1b[36m%s\x1b[0m", `🎃 - ${title}`);
-}
+async function otel() {}
