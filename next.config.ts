b7b3aa92af0e2cf5eca718ae95a0c8d704f95f18
Yuta Hiroto <git@hiroppy.me>
2025-01-30 07:54:00 +0900

    feat(next): add security headers

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,36 @@ config();
 
 const nextConfig: NextConfig = {
   /* start: sample */
+  async headers() {
+    return [
+      {
+        source: "/(.*?)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+        ],
+      },
+    ];
+  },
+
   images: {
     remotePatterns: [
       {
