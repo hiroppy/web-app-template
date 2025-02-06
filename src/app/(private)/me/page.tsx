03a9a3d5c73d54428517ed5aafe0b25600e5b08d
Yuta Hiroto <git@hiroppy.me>
2025-02-06 19:42:48 +0900

    feat: dynamicIO

--- src/app/(private)/me/page.tsx
+++ src/app/(private)/me/page.tsx
@@ -1,8 +1,17 @@
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 import { getSessionOrReject } from "../../_actions/auth";
 import { UpdateMyInfo } from "./_components/UpdateMyInfo";
 
 export default async function Page() {
+  return (
+    <Suspense fallback={<p>loading...</p>}>
+      <ValidateUser />
+    </Suspense>
+  );
+}
+
+async function ValidateUser() {
   const session = await getSessionOrReject();
 
   if (!session.success) {
