e983d01c7efed1f95a931ab1ec0511c8246e909c
Yuta Hiroto <git@hiroppy.me>
2025-02-09 12:06:04 +0900

    wip

--- src/app/_clients/stripe.ts
+++ src/app/_clients/stripe.ts
@@ -5,5 +5,5 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const successUrl =
   // TODO
-  `${process.env.NEXT_PUBLIC_SITE_URL}/api/payment/success?session_id={CHECKOUT_SESSION_ID}` as Route;
+  `${process.env.NEXT_PUBLIC_SITE_URL}/api/payment/checkout?session_id={CHECKOUT_SESSION_ID}` as Route;
 export const cancelUrl = process.env.NEXT_PUBLIC_WEBSITE_URL;
---
+++ src/app/api/payment/checkout/route.ts
@@ -0,0 +1,49 @@
+import { redirect } from "next/navigation";
+import { type NextRequest, NextResponse } from "next/server";
+import { auth } from "../../../_clients/nextAuth";
+import { prisma } from "../../../_clients/prisma";
+import { stripe } from "../../../_clients/stripe";
+
+export async function GET(req: NextRequest) {
+  const session = await auth();
+
+  if (!session) {
+    return NextResponse.json({ message: "error" }, { status: 401 });
+  }
+
+  const sessionId = req.nextUrl.searchParams.get("session_id");
+
+  if (!sessionId) {
+    return NextResponse.json({ message: "error" }, { status: 400 });
+  }
+
+  const paymentInfo = await stripe.checkout.sessions.retrieve(sessionId);
+
+  if (paymentInfo.status === "complete") {
+    try {
+      await prisma.$transaction(async (prisma) => {
+        await prisma.user.update({
+          where: {
+            id: session.user.id,
+          },
+          data: {
+            // TODO
+            stripeId: paymentInfo.customer as string,
+            subscriptions: {
+              create: {
+                subscriptionId: paymentInfo.subscription as string,
+                status: paymentInfo.status as string,
+              },
+            },
+          },
+        });
+      });
+    } catch (e) {
+      // TODO:
+    }
+
+    redirect("/");
+  } else {
+    redirect("/payment");
+  }
+}
